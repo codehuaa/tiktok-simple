125b4a7e8a457a6c99efbbc9aceaeb1aa48b6ff0
codehuaa <1055928999@qq.com>
2023-08-22 10:58:57 +0800

    初始化完成kitex框架

---
+++ kitex/handlers/user.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"context"
+	user "tiktok-simple/kitex/kitex_gen/user"
+)
+
+// UserServiceImpl implements the last service interface defined in the IDL.
+type UserServiceImpl struct{}
+
+// Register implements the UserServiceImpl interface.
+func (s *UserServiceImpl) Register(ctx context.Context, req *user.UserRegisterRequest) (resp *user.UserRegisterResponse, err error) {
+	// TODO: Your code here...
+	return
+}
+
+// Login implements the UserServiceImpl interface.
+func (s *UserServiceImpl) Login(ctx context.Context, req *user.UserLoginRequest) (resp *user.UserLoginResponse, err error) {
+	// TODO: Your code here...
+	return
+}
+
+// UserInfo implements the UserServiceImpl interface.
+func (s *UserServiceImpl) UserInfo(ctx context.Context, req *user.UserInfoRequest) (resp *user.UserInfoResponse, err error) {
+	// TODO: Your code here...
+	return
+}
