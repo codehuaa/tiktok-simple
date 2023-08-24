9f1a9aaae60c71e2c3b26080488c4b4f222dac5b
codehuaa <1055928999@qq.com>
2023-08-24 16:05:12 +0800

    fix:解决kitex开启多个微服务端口冲突问题

--- service/video/main.go
+++ service/video/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
 	video "tiktok-simple/kitex/kitex_gen/video/videoservice"
 	"tiktok-simple/service/video/service"
 )
 
 func main() {
-	svr := video.NewServer(new(service.VideoServiceImpl))
+	// 处理多个微服务时端口冲突的问题
+	addr, _ := net.ResolveTCPAddr("tcp", ":8082")
+	var opts []server.Option
+	opts = append(opts, server.WithServiceAddr(addr))
+
+	svr := video.NewServer(new(service.VideoServiceImpl), opts...)
 
 	err := svr.Run()
 
---
+++ startup.sh
@@ -0,0 +1,5 @@
+go run ./service/user/main.go
+go run ./service/video/main.go
+go run ./service/like/main.go
+go run ./service/comment/main.go
+go run ./http/main.go
